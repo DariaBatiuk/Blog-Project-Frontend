1c196df58546c006dd1e58ae12c552de1fa5b3cd
Daria <dariabatiuk5@gmail.com>
2023-08-17 21:17:34 -0300

    init commit

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { useForm } from 'react-hook-form';
+import styles from "./Login.module.scss";
+
+export const Login = () => {
+	const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+		mode: 'onChange',
+	});
+
+	const onSubmit = (values) => {
+		console.log(values);
+	}
+  return (
+		<Paper classes={{ root: styles.root }}>
+			<Typography classes={{ root: styles.title }} variant="h5">
+				Log in to your account
+			</Typography>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<TextField
+					className={styles.field}
+					label="E-Mail"
+					error={Boolean(errors.email?.message)}
+					helperText={errors.email?.message}
+					{...register('email', { required: 'Please write your email' })}
+					fullWidth
+				/>
+				<TextField
+					className={styles.field}
+					label="Password"
+					error={Boolean(errors.password?.message)}
+					helperText={errors.password?.message}
+					{...register('password', { required: 'Please write your password' })}
+					fullWidth
+				/>
+				<Button size="large" variant="contained" fullWidth type="submit">
+					Log in
+				</Button>
+			</form>
+		</Paper>
+	);
+	}	
