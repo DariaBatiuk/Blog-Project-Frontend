5e950c5dd1e5a44aec6e8def1ebc9b89da05dd97
Daria <dariabatiuk5@gmail.com>
2023-08-23 17:24:05 -0300

    added keys

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //  
 const  instance = axios.create({
-	baseURL: 'http://localhost:444'
+	baseURL: process.env.REACT_APP_API_URL
 });
 
 //on each request checking token and adding the token
